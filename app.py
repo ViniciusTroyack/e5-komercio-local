cf1a0fdddde97eff698a94378a6b53bb3c81dc09
Mônica S <monica.s.bergues@gmail.com>
2021-10-13 20:30:38 -0300

    chore: chore komercio local --> done

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
-# Cole seu código aqui
+from flask import Flask, request, jsonify
+from products import lista_de_produtos
+
+app = Flask(__name__)
+
+
+@app.route("/products", methods=["GET"])
+def list_products():
+    page = int(request.args['page']) if request.args['page'] is not None else 0
+    per_page = int(request.args['per_page']) if request.args['per_page'] is not None else 10
+
+    start = page*per_page
+    end = (page+1)*per_page
+
+    return jsonify(lista_de_produtos[start:end]), 200
+
+
+
+@app.route("/products/<id>", methods=["GET"])
+def get_product(id):
+    result = []
+    for item in lista_de_produtos:
+        if item['id'] == int(id):
+            result.append(item)
+
+    return jsonify(result), 200
