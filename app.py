f0fa058db20d06fc576b2480a3e56af2f5309c2a
Vinicius <troyackvinicius@gmail.com>
2021-10-15 18:53:56 -0300

    criando aplicacao completa com query e url dinamica

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
-# Cole seu código aqui
+from products import lista_de_produtos
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.get('/products')
+def list_products():
+
+    page = request.args.get('page')
+    per_page = request.args.get('per_page')
+
+    if page:
+        if per_page:
+            return jsonify(lista_de_produtos[:int(per_page)])
+
+    return jsonify(lista_de_produtos)
+
+@app.get('/products/<product_id>')
+def get_product(product_id):
+
+    output = [product for product in lista_de_produtos if product['id'] == int(product_id)]
+    
+    if len(output) == 0:
+        return jsonify('produto não encontrado'), 404
+
+    return jsonify(output)
